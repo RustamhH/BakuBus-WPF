49e866915ac6ed20952fff938235e025e7c48296
Rustam Hasanli <rustamh2006@gmail.com>
2023-07-11 19:44:10 +0400

    committ

--- BakuBus/obj/Debug/net6.0-windows/App.g.i.cs
+++ BakuBus/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace BakuBus {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/BakuBus;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BakuBus;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- BakuBus/obj/Debug/net6.0-windows/Views/MainView.g.i.cs
+++ BakuBus/obj/Debug/net6.0-windows/Views/MainView.g.i.cs
@@ -70,7 +70,7 @@ namespace Source.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BakuBus;component/views/mainview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BakuBus;V1.0.0.0;component/views/mainview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
